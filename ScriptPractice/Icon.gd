78f2159ca0827e935f653101ce71006cc1481d49
Ritik Singh <ritiksingh123456789@gmail.com>
2024-03-27 23:49:22 -0700

    Updated script to be controlled by user input

--- ScriptPractice/Icon.gd
+++ ScriptPractice/Icon.gd
@@ -4,7 +4,16 @@ var speed =400
 var angular_speed = PI
 
 func _process(delta):
-	rotation += angular_speed * delta
+	var direction = 0
+	if Input.is_action_pressed("ui_left"):
+		direction = -1
+	if Input.is_action_pressed("ui_right"):
+		direction = 1
 	
-	var velocity = Vector2.UP.rotated(rotation) * speed
+	rotation += angular_speed * direction * delta
+	
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_up"):
+		velocity = Vector2.UP.rotated(rotation) * speed
+		
 	position += velocity * delta
