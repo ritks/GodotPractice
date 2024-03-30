c621d67daa20e8b74db68f36aafe03c9afc8bc21
Ritik Singh <ritiksingh123456789@gmail.com>
2024-03-30 13:25:19 -0700

    Changes to new C# scripts, added HUD script in C#

--- First2D/mob.cs
+++ First2D/mob.cs
@@ -2,17 +2,17 @@ using Godot;
 
 public partial class Mob : RigidBody2D
 {
-    // Don't forget to rebuild the project.
+	// Don't forget to rebuild the project.
 }
 
 public override void _Ready()
 {
-    var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-    string[] mobTypes = animatedSprite2D.SpriteFrames.GetAnimationNames();
-    animatedSprite2D.Play(mobTypes[GD.Randi() % mobTypes.Length]);
+	var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+	string[] mobTypes = animatedSprite2D.SpriteFrames.GetAnimationNames();
+	animatedSprite2D.Play(mobTypes[GD.Randi() % mobTypes.Length]);
 }
 
 private void OnVisibleOnScreenNotifier2DScreenExited()
 {
-    QueueFree();
+	QueueFree();
 }
