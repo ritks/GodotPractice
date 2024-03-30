230aba357b699c7a03a39e3c16121234e55c3238
Ritik Singh <ritiksingh123456789@gmail.com>
2024-03-29 22:59:24 -0700

    Finished First2D game and added playable exe file

--- First2D/player.gd
+++ First2D/player.gd
@@ -1,8 +1,8 @@
 extends Area2D
 signal hit
 
-@export var speed = 400
-var screen_size
+@export var speed = 400 # How fast the player will move (pixels/sec).
+var screen_size # Size of the game window.
 
 
 # Called when the node enters the scene tree for the first time.
@@ -13,15 +13,15 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var velocity = Vector2.ZERO
+	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
 	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
@@ -35,6 +35,7 @@ func _process(delta):
 	if velocity.x != 0:
 		$AnimatedSprite2D.animation = "walk"
 		$AnimatedSprite2D.flip_v = false
+		# See the note below about boolean assignment.
 		$AnimatedSprite2D.flip_h = velocity.x < 0
 	elif velocity.y != 0:
 		$AnimatedSprite2D.animation = "up"
@@ -42,10 +43,12 @@ func _process(delta):
 
 
 func _on_body_entered(body):
-	hide()
+	hide() # Player disappears after being hit.
 	hit.emit()
+	# Must be deferred as we can't change physics properties on a physics callback.
 	$CollisionShape2D.set_deferred("disabled", true)
 	
-func _start(pos):
+func start(pos):
 	position = pos
+	show()
 	$CollisionShape2D.disabled = false
