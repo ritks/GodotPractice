794cc81443376c7ee16ed0d74731456f8eabda48
Ritik Singh <ritiksingh123456789@gmail.com>
2024-04-02 21:15:12 -0700

    Testing diagonal dashing

--- CelesteClonePractice/Player.cs
+++ CelesteClonePractice/Player.cs
@@ -10,13 +10,13 @@ public partial class Player : CharacterBody2D
 	private const int WalkMaxSpeed = 5000;
 	private const int WalkForce = 7500;
 	private const int StopForce = 2000;
-	private const int JumpSpeed = 4000; 
+	private const int JumpSpeed = 4000;
 	private const int DashSpeed = 3500;
 	bool canDash = true;
 	public override void _Ready()
 	{
 		ScreenSize = GetViewportRect().Size;
-	
+
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -26,39 +26,72 @@ public partial class Player : CharacterBody2D
 	}
 
 	public override void _PhysicsProcess(double delta)
-    {
+	{
 
 		//Basic velocity and movement
 		var velocity = Vector2.Zero;
 		var walk = WalkForce * Input.GetAxis("move_left", "move_right");
-		
-		if(Mathf.Abs(walk) < WalkForce * 0.2){
+
+		if (Mathf.Abs(walk) < WalkForce * 0.2)
+		{
 			velocity.X = Mathf.MoveToward(velocity.X, 0, StopForce * (float)delta);
 		}
-		else{
+		else
+		{
 			velocity.X += walk * (float)delta;
 		}
 		velocity.X = Mathf.Clamp(velocity.X, -1 * WalkMaxSpeed, WalkMaxSpeed);
 
 		//Applying gravity at every frame
 		velocity.Y += (float)delta * Gravity * 5;
-		
+
 		//Checking if on floor to jump
-		if(IsOnFloor() && Input.IsActionJustPressed("move_up")){
+		if (IsOnFloor() && Input.IsActionJustPressed("move_up"))
+		{
 			velocity.Y = -JumpSpeed;
 		}
 
 		//Making dash mechanic
 		// bool canDash = true;
-		if((canDash || IsOnFloor()) && Input.IsActionPressed("move_right") && Input.IsActionJustPressed("dash")){
+		if (canDash && Input.IsActionPressed("move_right") && Input.IsActionJustPressed("dash"))
+		{
 			velocity.X += DashSpeed;
-			canDash = !IsOnFloor() ? canDash = false : canDash = true;
+			canDash = false;
+
+		}
+		if (canDash && Input.IsActionPressed("move_left") && Input.IsActionJustPressed("dash"))
+		{
+			velocity.X -= DashSpeed;
+			canDash = false;
 
 		}
 
+		// NEED TO TEST DASHING UP WIHTOUT DIAGONAL MOVEMENT YET
+		if (canDash && Input.IsActionPressed("move_right")
+			&& Input.IsActionPressed("move_up") && Input.IsActionJustPressed("dash"))
+		{
+			velocity.X += DashSpeed;
+			velocity.Y -= DashSpeed;
+			velocity += velocity.Normalized() * DashSpeed;
+
+			canDash = false;
+
+		}
+
+		// if (canDash && Input.IsActionPressed("move_up") && Input.IsActionJustPressed("dash"))
+		// {
+		// 	velocity.Y -= DashSpeed;
+		// 	canDash = false;
+		// }
+
+
+		if (IsOnFloor())
+			canDash = true;
+
+
 		Velocity = velocity;
-        MoveAndSlide();
-		
+		MoveAndSlide();
+
 	}
 
 	public void Start(Vector2 position)
